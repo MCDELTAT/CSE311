d4e25c5ba19a662aa98516dcf548a477b0614bac
Unknown <aaronjchamberlain@gmail.com>
2017-01-22 18:59:38 -0800

    Deleted Junk files, added gitignore

--- lab1/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/aaron/Git Repos/CSE311/lab1/bi_directional_shift_tb_isim_beh.exe" -prj "/home/aaron/Git Repos/CSE311/lab1/bi_directional_shift_tb_beh.prj" "work.bi_directional_shift_tb" "work.glbl" 
